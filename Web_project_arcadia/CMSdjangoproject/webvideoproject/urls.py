5bd7eac733293473d477fd7c263c472c15984138
langtucodon9stv <langtuphieudu.stv@gmail.com>
2017-03-23 08:59:01 +0300

    khong co gi ca

--- Web_project_arcadia/CMSdjangoproject/webvideoproject/urls.py
+++ Web_project_arcadia/CMSdjangoproject/webvideoproject/urls.py
@@ -22,6 +22,7 @@ urlpatterns = [
 urlpatterns += i18n_patterns(
     url(r'^admin/', include(admin.site.urls)),  # NOQA
     url(r'^', include('cms.urls')),
+
 )
 
 # This is only needed when using runserver.
